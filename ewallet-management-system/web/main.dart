cf8e1c44dced241d5a3dea3e4d57f0336fc1b156
Luqman Malik <hakim19jan@gmail.com>
2024-05-01 22:34:33 +0800

    Added getAllTransactions function for admin

--- ewallet-management-system/web/main.dart
+++ ewallet-management-system/web/main.dart
@@ -93,24 +93,6 @@ void main() {
     ));
   }
 
-  // Admin functions
-
-  void editTransaction(int index, String description, double amount,
-      DateTime date, double? discount, double? cashback, String user) {
-    transactions[index] = Transaction(
-      description: description,
-      amount: amount,
-      date: date,
-      discount: discount,
-      cashback: cashback,
-      user: user,
-    );
-  }
-
-  void deleteTransaction(int index) {
-    transactions.removeAt(index);
-  }
-
   dynamic getUserTransactions(String user) {
     var userTransactions = transactions.where((t) => t.user == user).toList();
     // Calculate balance for each transaction
@@ -132,6 +114,28 @@ void main() {
       });
     }
     return calculatedTransactions;
+  }
+
+  // Admin functions
+
+  void editTransaction(int index, String description, double amount,
+      DateTime date, double? discount, double? cashback, String user) {
+    transactions[index] = Transaction(
+      description: description,
+      amount: amount,
+      date: date,
+      discount: discount,
+      cashback: cashback,
+      user: user,
+    );
+  }
+
+  void deleteTransaction(int index) {
+    transactions.removeAt(index);
+  }
+
+  dynamic getAllTransactions() {
+    return transactions;
   }
 
   button.onClick.listen((event) {

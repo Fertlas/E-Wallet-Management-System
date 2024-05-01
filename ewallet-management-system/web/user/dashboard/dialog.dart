330122e3c0433ea9fcb9c10e1b60bd52009f4b48
syamilu <wanmuhammadsyamil@gmail.com>
2024-05-01 18:07:56 +0800

    :sparkle: dashboard and login page updated

--- ewallet-management-system/web/user/dashboard/dialog.dart
+++ ewallet-management-system/web/user/dashboard/dialog.dart
@@ -1,6 +1,11 @@
 import 'dart:html';
 
 void main() {
+  handleDialog();
+  handleFormSubmission();
+}
+
+void handleDialog() {
   final ButtonElement newTransc =
       querySelector('#new-transaction') as ButtonElement;
   final DialogElement dialog =
@@ -14,5 +19,18 @@ void main() {
 
   cancelButton.onClick.listen((event) {
     dialog.close();
+  });
+}
+
+void handleFormSubmission() {
+  final FormElement form = querySelector('#add-transaction') as FormElement;
+  final InputElement typeInput = querySelector('#type') as InputElement;
+  final InputElement amountInput = querySelector('#amount') as InputElement;
+
+  form.onSubmit.listen((event) {
+    event.preventDefault();
+    String? type = typeInput.value;
+    String? amount = amountInput.value;
+    print('Type: $type, Price: $amount');
   });
 }
